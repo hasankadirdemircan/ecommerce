c57b706f266dcca37a64cfd4c688e7a3279dbeb3
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-08-04 11:52:11 +0300

    deleted empty lines

--- src/main/java/com/cornershop/ecommerce/model/Order.java
+++ src/main/java/com/cornershop/ecommerce/model/Order.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,5 +33,4 @@ public class Order {
     private Double price;
 
     private LocalDateTime purchaseDate;
-
 }
